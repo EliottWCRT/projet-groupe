e91f66ec436d2917139ff4041437de4e5218172a
test-Melodie <melodiebellot@gmail.com>
2023-05-03 12:22:32 +0200

    ajout blog

---
+++ projet-de-groupe/resources/views/articles.blade.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+	<title>Blog d'un groupe de musique</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+      <!--Contenu-->
+    <h1 class="text-center">Blog</h1>
+    <section class="article">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
+                <h2>Article 1</a></h2>
+                <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item">Modifier</a>
+                  <a class="dropdown-item">Supprimer</a>
+                </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+            </div>
+            <div class="col-md-4">
+              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
+                <h2>Article 2</a></h2>
+                <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item">Modifier</a>
+                  <a class="dropdown-item">Supprimer</a>
+                </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+            </div>
+            <div class="col-md-4">
+              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
+                <h2>Article 3</a></h2>
+                <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item">Modifier</a>
+                  <a class="dropdown-item">Supprimer</a>
+                </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+            </div>
+            <div class="col-md-4">
+              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
+                <h2>Article 4</a></h2>
+                <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item">Modifier</a>
+                  <a class="dropdown-item">Supprimer</a>
+                </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>            
+              </div>
+          </div>
+        </div>
+      </section>
+      <!--Fin contenu-->  
+      <footer class="bg-dark text-light py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>© 2023 Blog. Tous droits réservés.</p>
+      </div>
+      <div class="col-md-6">
+        <ul class="list-inline text-md-end">
+          <li class="list-inline-item"><a href="welcome.blade.php">Accueil</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+    
+</body>
+</html>
--- projet-de-groupe/resources/views/dashboard.blade.php
+++ projet-de-groupe/resources/views/dashboard.blade.php
@@ -1,15 +1,58 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+         <!--Barre de navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Nom du groupe</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            @endguest
+        </ul>
+    </div>
+</nav>
+      <!--Fin Barre de navigation-->
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @include('articles')
                 </div>
             </div>
         </div>
