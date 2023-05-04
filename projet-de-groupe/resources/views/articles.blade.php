b9ff5c317404cb43599c6faab3b16031374e000a
EliottWCRT <128164554+EliottWCRT@users.noreply.github.com>
2023-05-04 15:07:55 +0200

    firstcommit

--- projet-de-groupe/resources/views/articles.blade.php
+++ projet-de-groupe/resources/views/articles.blade.php
@@ -12,57 +12,162 @@
 <body>
       <!--Contenu-->
     <h1 class="text-center">Blog</h1>
+    <br>
     <section class="article">
         <div class="container">
           <div class="row">
-            <div class="col-md-4">
-              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
-                <h2>Article 1</a></h2>
-                <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item">Modifier</a>
-                  <a class="dropdown-item">Supprimer</a>
+            <div class="col-md-4" class="card">
+              <img src="images/IMG_3370.JPG" class="card-img-top" alt="Image">
+              <div class="card-body">
+                <h5 class="card-title">Article 1</h5>
+                @guest
+                @else
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Options
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item">Modifier</a>
+                            <a class="dropdown-item">Supprimer</a>
+                        </div>
+                    </div>
+                @endguest
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+                <div class="form-group">
+                  @guest
+      @else
+      <label for="comment">Commenter :</label>
+      @endguest
+                  @guest
+                  @else
+                  <textarea class="form-control" id="comment" rows="3"></textarea>
+                  @endguest
                 </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
-            </div>
-            <div class="col-md-4">
-              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
-                <h2>Article 2</a></h2>
-                <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item">Modifier</a>
-                  <a class="dropdown-item">Supprimer</a>
-                </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
-            </div>
-            <div class="col-md-4">
-              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
-                <h2>Article 3</a></h2>
-                <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item">Modifier</a>
-                  <a class="dropdown-item">Supprimer</a>
-                </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
-            </div>
-            <div class="col-md-4">
-              <img src="images/IMG_3370.JPG" alt="Image" class="img-fluid">
-                <h2>Article 4</a></h2>
-                <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>  
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item">Modifier</a>
-                  <a class="dropdown-item">Supprimer</a>
-                </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>            
+                @guest
+                  @else
+                <button type="button" class="btn btn-primary">Envoyer</button>
+                @endguest
               </div>
+              <div class="card-footer">
+                <h6>Commentaires:</h6>
+                <ul class="list-group list-group-flush">
+                </ul>
+              </div>
+            </div>
+<div class="col-md-4" class="card">
+  <img src="images/IMG_3370.JPG" class="card-img-top" alt="Image">
+  <div class="card-body">
+    <h5 class="card-title">Article 2</h5>
+    @guest
+    @else
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Options
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item">Modifier</a>
+                <a class="dropdown-item">Supprimer</a>
+            </div>
+        </div>
+    @endguest
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+    <div class="form-group">
+      @guest
+      @else
+      <label for="comment">Commenter :</label>
+      @endguest
+      @guest
+                  @else
+                  <textarea class="form-control" id="comment" rows="3"></textarea>
+                  @endguest
+                </div>
+                @guest
+                  @else
+                <button type="button" class="btn btn-primary">Envoyer</button>
+                @endguest
+  </div>
+  <div class="card-footer">
+    <h6>Commentaires:</h6>
+    <ul class="list-group list-group-flush">
+    </ul>
+  </div>
+</div>
+<div class="col-md-4" class="card">
+  <img src="images/IMG_3370.JPG" class="card-img-top" alt="Image">
+  <div class="card-body">
+    <h5 class="card-title">Article 3</h5>
+    @guest
+    @else
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Options
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item">Modifier</a>
+                <a class="dropdown-item">Supprimer</a>
+            </div>
+        </div>
+    @endguest
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+    <div class="form-group">
+      @guest
+      @else
+      <label for="comment">Commenter :</label>
+      @endguest
+      @guest
+                  @else
+                  <textarea class="form-control" id="comment" rows="3"></textarea>
+                  @endguest
+                </div>
+                @guest
+                  @else
+                <button type="button" class="btn btn-primary">Envoyer</button>
+                @endguest
+  </div>
+  <div class="card-footer">
+    <h6>Commentaires:</h6>
+    <ul class="list-group list-group-flush">
+    </ul>
+  </div>
+</div>
+<div class="col-md-4" class="card">
+  <img src="images/IMG_3370.JPG" class="card-img-top" alt="Image">
+  <div class="card-body">
+    <h5 class="card-title">Article 4</h5>
+    @guest
+    @else
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Options
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item">Modifier</a>
+                <a class="dropdown-item">Supprimer</a>
+            </div>
+        </div>
+    @endguest
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, justo quis commodo consectetur, eros tortor pharetra turpis, sit amet aliquet neque massa id quam.</p>
+    <div class="form-group">
+      @guest
+      @else
+      <label for="comment">Commenter :</label>
+      @endguest
+      @guest
+      @else
+      <textarea class="form-control" id="comment" rows="3"></textarea>
+      @endguest
+    </div>
+    @guest
+      @else
+    <button type="button" class="btn btn-primary">Envoyer</button>
+    @endguest
+  </div>
+  <div class="card-footer">
+    <h6>Commentaires:</h6>
+    <ul class="list-group list-group-flush">
+    </ul>
+  </div>
+</div>
           </div>
         </div>
       </section>
@@ -73,14 +178,32 @@
       <div class="col-md-6">
         <p>© 2023 Blog. Tous droits réservés.</p>
       </div>
-      <div class="col-md-6">
-        <ul class="list-inline text-md-end">
-          <li class="list-inline-item"><a href="welcome.blade.php">Accueil</a></li>
-        </ul>
-      </div>
     </div>
   </div>
 </footer>
-    
+<script>
+  // Sélectionne le bouton "Envoyer"
+  const sendButton = document.querySelector('button.btn-primary');
+
+  // Ajoute un écouteur d'événements sur le clic du bouton "Envoyer"
+  sendButton.addEventListener('click', () => {
+    // Récupère le contenu du champ de texte
+    const commentContent = document.querySelector('#comment').value;
+
+    // Sélectionne la liste de commentaires existante
+    const commentList = document.querySelector('.list-group');
+
+    // Crée un nouvel élément de liste contenant le commentaire
+    const newComment = document.createElement('li');
+    newComment.classList.add('list-group-item');
+    newComment.textContent = commentContent;
+
+    // Ajoute le nouvel élément de liste à la fin de la liste existante
+    commentList.appendChild(newComment);
+
+    // Réinitialise le champ de texte
+    document.querySelector('#comment').value = '';
+  });
+</script>
 </body>
 </html>
